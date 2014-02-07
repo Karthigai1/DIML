d0017deb954fb4dfbb763b91c364b276d0c50cd5
Karthigai1 <karthigai.devi@kairosindia.com>
2014-02-07 11:41:59 +0530

    Add batch_entry.php

---
+++ batch_entry.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+        <meta charset="utf-8" />
+        <title>Batch Entry</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link href="/css/core.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+<section id="wrapper">  
+        
+        <form action="check_file.php" method="post" enctype="multipart/form-data">
+        
+                <table cellpadding="0" cellspacing="0" border="0" class="table">
+                        <tr>
+                                <th><label for="file"></label></th>
+                        </tr>
+                        <tr>
+                                <td><input type="file" name="file" id="file" size="30" /></td>
+                  </tr>
+                        <tr>
+                                <td><input type="submit" id="btn" class="fl_l" value="Submit" /></td>
+                        </tr>
+                </table>
+                
+  </form>
+        
+</section>
+
+</body>
+</html>
